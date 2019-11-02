dd0a0d73735fde52db9b982303bd38bf63d9afd1
rishabh <deliberatethink@gmail.com>
2019-11-02 13:45:24 +0530

    Update and rename last28DaysGen.md to last28DaysGen.sql

---
+++ last28DaysGen.sql
@@ -0,0 +1,10 @@
+
+select a.Date 
+from (
+    select curdate() - INTERVAL (a.a + (10 * b.a) + (100 * c.a) + (1000 * d.a) ) DAY as Date
+    from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
+    cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
+    cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
+    cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as d
+) a
+where a.Date between CURDATE() - INTERVAL 28 DAY AND CURDATE()
